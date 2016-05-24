a7f246f63bb8d69f57c9d16544bbd14829e61db5
Ian Denhardt <ian@zenhack.net>
2016-05-24 18:55:51 -0400

    Basic proxying functionality + run outside sandstorm

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"golang.org/x/net/context"
+	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"time"
@@ -11,11 +14,75 @@ import (
 	"zenhack.net/go/sandstorm/websession"
 )
 
+type Config struct {
+	Listen string `json:"listen"`
+	Dial   string `json:"dial"`
+}
+
+func checkFatal(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello!")
 	})
+	if os.Getenv("SANDSTORM") == "1" {
+		sandstormMain()
+	} else {
+		normalMain()
+	}
+}
 
+func readConfig(filename string) (*Config, error) {
+	var config Config
+	file, err := os.Open(filename)
+	checkFatal(err)
+	defer file.Close()
+	d := json.NewDecoder(file)
+	err = d.Decode(&config)
+	checkFatal(err)
+	return &config, err
+}
+
+func doCopy(ch chan<- bool, dst io.Writer, src io.Reader) {
+	io.Copy(dst, src)
+	ch <- false
+}
+
+func serve(config *Config) {
+	l, err := net.Listen("tcp", config.Listen)
+	checkFatal(err)
+	for {
+		clientConn, err := l.Accept()
+		if err != nil {
+			// TODO: handle this? net/http does some backoff stuff, need to
+			// investigate/understand that better.
+			continue
+		}
+		srvConn, err := net.Dial("tcp", config.Dial)
+		ch := make(chan bool, 2)
+		go doCopy(ch, clientConn, srvConn)
+		go doCopy(ch, srvConn, clientConn)
+		<-ch
+		<-ch
+		// TODO: should keep the server connection open. Need to wrangle
+		// something to deal with the traffic while the client is offline.
+		clientConn.Close()
+		srvConn.Close()
+	}
+}
+
+func normalMain() {
+	config, err := readConfig("config.json")
+	checkFatal(err)
+	serve(config)
+}
+
+func sandstormMain() {
 	ctx := context.Background()
 	api, err := grain.ConnectAPI(ctx, websession.FromHandler(http.DefaultServeMux))
 
