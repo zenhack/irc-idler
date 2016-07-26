d6cf48ab38a4e8fac15eaa0cad973918cebf0552
Ian Denhardt <ian@zenhack.net>
2016-07-26 17:47:03 -0400

    Add TLS support (when connecting to the server)

--- cmd/irc-idler/main.go
+++ cmd/irc-idler/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"golang.org/x/net/proxy"
@@ -14,7 +15,16 @@ var (
 	laddr    = flag.String("laddr", ":6667", "Local address to listen on")
 	raddr    = flag.String("raddr", "", "Remote address to connect to")
 	debuglog = flag.Bool("debuglog", false, "Enable debug logging.")
+
+	// TODO: default should probably be `true`.
+	useTLS = flag.Bool("tls", false, "Connect via tls.")
 )
+
+type TLSDialer tls.Config
+
+func (cfg *TLSDialer) Dial(network, addr string) (net.Conn, error) {
+	return tls.Dial(network, addr, (*tls.Config)(cfg))
+}
 
 func checkFatal(err error) {
 	if err != nil {
@@ -28,10 +38,16 @@ func checkFatal(err error) {
 func main() {
 	flag.Parse()
 	var logger *log.Logger
+	var dialer proxy.Dialer
 	if *debuglog {
 		logger = log.New(os.Stderr, log.Prefix(), log.Flags())
 	}
+	if *useTLS {
+		dialer = (*TLSDialer)(nil)
+	} else {
+		dialer = proxy.Direct
+	}
 	l, err := net.Listen("tcp", *laddr)
 	checkFatal(err)
-	ircproxy.NewProxy(l, proxy.Direct, *raddr, logger).Run()
+	ircproxy.NewProxy(l, dialer, *raddr, logger).Run()
 }
