91d85dac094a286b6996f7b08f6d0b90bb1abdda
Ian Denhardt <ian@zenhack.net>
2016-07-26 17:04:55 -0400

    Be more flexible with client identifiers
    See the stuff added to protocol-hacks for a full explanation.

--- doc/protocol-hacks.md
+++ doc/protocol-hacks.md
@@ -12,5 +12,19 @@ Clients SHOULD send a NICK message followed by a USER message, but some
 clients (notably pidgin) swap these. We therefore accept these in either
 order.
 
+# RPL_WELCOME client identifier
+
+Section 3.1 specifies that the server's RPL_WELCOME message includes the
+full client identifier. The term "client identifier" is not explicitly
+defined in this section, and does not appear anywhere else in the
+document. However, section 5.1 shows an example RPL_WELCOME message:
+
+   001    RPL_WELCOME
+      "Welcome to the Internet Relay Network
+       <nick>!<user>@<host>"
+
+Which leads one to believe that a client identifier is
+`<nick>!<user>@<host>`. However, both OFTC and freenode return just the
+`<nick>` portion. We're flexible with what we accept here.
 
 [rfc]: https://www.rfc-editor.org/rfc/rfc2812.txt
--- irc/clientid.go
+++ irc/clientid.go
@@ -10,28 +10,31 @@ type ClientID struct {
 }
 
 func (id ClientID) String() string {
-	return id.Nick + "!" + id.User + "@" + id.Host
+	if id.User == "" || id.Host == "" {
+		return id.Nick
+	} else {
+		return id.Nick + "!" + id.User + "@" + id.Host
+	}
 }
 
 type clientIDParseError string
 
 func (e clientIDParseError) Error() string {
-	return fmt.Sprintf(
-		"Error parsing client id: there must be exactly one %q character.",
-		string(e),
-	)
+	return fmt.Sprintf("Error parsing client id: %v", string(e))
 }
 
 func ParseClientID(text string) (ClientID, error) {
 	var ret ClientID
 	parts := strings.Split(text, "!")
-	if len(parts) != 2 {
-		return ClientID{}, clientIDParseError("!")
+	if len(parts) == 1 {
+		return ClientID{parts[0], "", ""}, nil
+	} else if len(parts) != 2 {
+		return ClientID{}, clientIDParseError("More than one '!' character")
 	}
 	ret.Nick = parts[0]
 	parts = strings.Split(parts[1], "@")
 	if len(parts) != 2 {
-		return ClientID{}, clientIDParseError("@")
+		return ClientID{}, clientIDParseError("Have user field but no host.")
 	}
 	ret.User = parts[0]
 	ret.Host = parts[1]
