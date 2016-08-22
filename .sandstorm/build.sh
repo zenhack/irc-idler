07dc0a296b618a05c184a382506c18db64b04e99
Ian Denhardt <ian@zenhack.net>
2016-08-22 19:48:10 -0400

    Don't build the app in the VM
    The version of go available in the Jessie VM is ancient (1.3.x), and is
    missing some features I want from newer releases of go.

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-export GOPATH="/opt/app/.sandstorm/gopath"
-
-deps='
-	golang.org/x/net/context
-	zenhack.net/go/sandstorm/grain
-'
-
-for d in $deps; do
-	[ -e $GOPATH/src/$d ] || go get $d
-done
-
-cd /opt/app/cmd/irc-idler-sandstorm
-go build
 exit 0
