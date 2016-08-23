c987788c143e7ad4a3229b58e2a3389a2817e7ae
Ian Denhardt <ian@zenhack.net>
2016-08-23 00:35:14 -0400

    Redo routing with gorilla's mux package
    A bit cleaner; no need to do ifs for the path/method.

--- sandstorm/webui/webui.go
+++ sandstorm/webui/webui.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"errors"
+	"github.com/gorilla/mux"
 	"golang.org/x/net/xsrftoken"
 	"html/template"
 	"io"
@@ -53,79 +54,65 @@ func checkXSRF(key, userID, actionID string, req *http.Request) error {
 }
 
 func NewHandler(backend *Backend) (http.Handler, error) {
-	serveMux := http.NewServeMux()
+	r := mux.NewRouter()
 	// TODO: might make sense to not generate this on every startup:
 	xsrfKey, err := genXSRFKey()
 	if err != nil {
 		return nil, err
 	}
 
-	serveMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		if req.URL.Path != "/" {
-			// The way ServeMux's routing works we can't easily specify a
-			// Handler that *just* covers the root, so we do this instead.
-			w.WriteHeader(404)
-			w.Write([]byte("Not found"))
-			return
-		}
-		token := xsrftoken.Generate(
-			xsrfKey,
-			"TODO",
-			"/proxy-settings/",
-		)
-		tplCtx := struct{ XSRFToken string }{token}
-		indexTpl.Execute(w, &tplCtx)
-	})
+	r.Methods("GET").Path("/").
+		HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			token := xsrftoken.Generate(
+				xsrfKey,
+				"TODO",
+				"/proxy-settings/",
+			)
+			tplCtx := struct{ XSRFToken string }{token}
+			indexTpl.Execute(w, &tplCtx)
+		})
 
-	serveMux.Handle("/static/", http.FileServer(http.Dir(staticPath)))
+	r.Methods("GET").PathPrefix("/static/").Handler(http.FileServer(http.Dir(staticPath)))
 
-	serveMux.HandleFunc("/proxy-settings/", func(w http.ResponseWriter, req *http.Request) {
-		if req.Method != "POST" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte("Method not allowed.\n"))
-			return
-		}
-		if err := checkXSRF(xsrfKey, "TODO", "/proxy-settings/", req); err != nil {
-			w.WriteHeader(400)
-			w.Write([]byte(err.Error()))
-			return
-		}
-		port, err := strconv.ParseUint(req.FormValue("port"), 10, 16)
-		if err != nil {
-			w.WriteHeader(400)
-			w.Write([]byte(err.Error()))
-			return
-		}
-		if port == 0 {
-			w.WriteHeader(400)
-			w.Write([]byte("Port must be non-zero"))
-			return
-		}
-		backend.ServerConfigs <- ServerConfig{
-			Host: req.FormValue("host"),
-			Port: uint16(port),
-		}
-	})
+	r.Methods("POST").Path("/proxy-settings").
+		HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			if err := checkXSRF(xsrfKey, "TODO", "/proxy-settings/", req); err != nil {
+				w.WriteHeader(400)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			port, err := strconv.ParseUint(req.FormValue("port"), 10, 16)
+			if err != nil {
+				w.WriteHeader(400)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			if port == 0 {
+				w.WriteHeader(400)
+				w.Write([]byte("Port must be non-zero"))
+				return
+			}
+			backend.ServerConfigs <- ServerConfig{
+				Host: req.FormValue("host"),
+				Port: uint16(port),
+			}
+		})
 
-	serveMux.HandleFunc("/ip-network-cap", func(w http.ResponseWriter, req *http.Request) {
-		if req.Method != "POST" {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			w.Write([]byte("Method not allowed.\n"))
-			return
-		}
-		// Size is mostly arbitrary. This is way bigger than we
-		// actually need, but it's still tiny and means we don't
-		// have to think to see that it's big enough:
-		limitedBody := io.LimitReader(req.Body, 512)
+	r.Methods("POST").Path("/ip-network-cap").
+		HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			// Size is mostly arbitrary. This is way bigger than we
+			// actually need, but it's still tiny and means we don't
+			// have to think to see that it's big enough:
+			limitedBody := io.LimitReader(req.Body, 512)
 
-		dec := base64.NewDecoder(base64.RawURLEncoding, limitedBody)
-		buf, err := ioutil.ReadAll(dec)
-		if err != nil {
-			println(err.Error())
-			w.WriteHeader(400)
-			return
-		}
-		backend.IpNetworkCaps <- buf
-	})
-	return serveMux, nil
+			dec := base64.NewDecoder(base64.RawURLEncoding, limitedBody)
+			buf, err := ioutil.ReadAll(dec)
+			if err != nil {
+				println(err.Error())
+				w.WriteHeader(400)
+				return
+			}
+			backend.IpNetworkCaps <- buf
+		})
+	return r, nil
 }
