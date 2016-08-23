6906354106224f7c25788fd0af2485af74ab740b
Ian Denhardt <ian@zenhack.net>
2016-08-23 17:57:08 -0400

    vagrant-spk: Don't install build stuff.
    ...Since we're building outside the VM again anyway

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -4,14 +4,4 @@
 # - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
 
 set -euo pipefail
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y golang git
-
-export GOPATH="/opt/app/.sandstorm/gopath"
-_srcdir="$GOPATH/src/zenhack.net/go/irc-idler"
-[ -L "$_srcdir" ] || {
-	mkdir -p $(dirname $_srcdir) || true
-	ln -s /opt/app  $_srcdir
-}
 exit 0
