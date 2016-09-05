4db37ffd7ab6fe2610af246ac257ee3e9d1a2399
Ian Denhardt <ian@zenhack.net>
2016-09-04 21:04:48 -0400

    Add a regression test for issue #4

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -3,6 +3,7 @@ package proxy
 import (
 	"testing"
 	"time"
+	"zenhack.net/go/irc-idler/irc"
 )
 
 func TestConnectDisconnect(t *testing.T) {
@@ -13,6 +14,22 @@ func TestConnectDisconnect(t *testing.T) {
 		ClientDisconnect{},
 		// Handshake isn't done:
 		DropServer{},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Regression tests for https://github.com/zenhack/irc-idler/issues/4
+func TestNickInUse(t *testing.T) {
+	state := StartTestProxy()
+	err := Expect(state, time.Second,
+		ClientConnect{},
+		ConnectServer{},
+		&FromClient{Command: "NICK", Params: []string{"alice"}},
+		&ToServer{Command: "NICK", Params: []string{"alice"}},
+		&FromServer{Command: irc.ERR_NICKNAMEINUSE},
+		&ToClient{Command: irc.ERR_NICKNAMEINUSE},
 	)
 	if err != nil {
 		t.Fatal(err)
