980362133b397309032070bab61b70a6885b448b
Ian Denhardt <ian@zenhack.net>
2016-09-06 16:01:59 -0400

    Clean up parsing of ClientIDs

--- doc/protocol-hacks.md
+++ doc/protocol-hacks.md
@@ -3,8 +3,8 @@ behavior of software out in the wild sometimes differs, and the RFC is a
 valiant, but not always successful attempt at documenting that behavior.
 This document indexes the various workarounds we've had to implement in
 order to deal with other software that doesn't follow the spec
-perfectly, as well as just general inconsistencies and how we deal with
-them.
+perfectly, as well general inconsistencies and things that are poorly
+described and how we deal with them.
 
 # USER/NICK ordering.
 
@@ -25,8 +25,10 @@ document. However, section 5.1 shows an example RPL_WELCOME message:
        <nick>!<user>@<host>"
 
 Which leads one to believe that a client identifier is
-`<nick>!<user>@<host>`. However, both OFTC and freenode return just the
-`<nick>` portion. We're flexible with what we accept here.
+`<nick>!<user>@<host>`. This looks roughly like one of the variants of
+the "prefix" production in section 2.3.1, so that's the syntax we
+accept. Both OFTC and freenode return just the `<nick>` portion, but
+since this is allowed according to that
 
 [rfc]: https://www.rfc-editor.org/rfc/rfc2812.txt
 
--- irc/clientid.go
+++ irc/clientid.go
@@ -5,19 +5,24 @@ import (
 	"strings"
 )
 
-// A client identifier. The RFC suggests (but actually doesn't explicitly say)
-// that this is of the form `<nick>!<user>@<host>`. We accept either this or
-// just `<nick>` in which case the value of the other two fields will be `""`.
+// A client identifier. RFC 2812 defines the syntax of this in section 2.3.1,
+// in the latter half of the "prefix" production. The description of RPL_WELCOME
+// suggests (but actually doesn't explicitly say) that this is the syntax for
+// its client field.
 type ClientID struct {
 	Nick, User, Host string
 }
 
 func (id ClientID) String() string {
-	if id.User == "" || id.Host == "" {
-		return id.Nick
-	} else {
-		return id.Nick + "!" + id.User + "@" + id.Host
+	ret := id.Nick
+	if id.Host == "" {
+		return ret
 	}
+	if id.User != "" {
+		ret += "!" + id.User
+	}
+	ret += "@" + id.Host
+	return ret
 }
 
 type clientIDParseError string
@@ -28,21 +33,37 @@ func (e clientIDParseError) Error() string {
 
 func ParseClientID(text string) (ClientID, error) {
 	var ret ClientID
-	parts := strings.Split(text, "!")
-	if parts[0] == "" {
+	nickHostParts := strings.Split(text, "@")
+	switch len(nickHostParts) {
+	case 1:
+		ret = ClientID{Nick: text}
+	case 2:
+		nickUserParts := strings.Split(nickHostParts[0], "!")
+		switch len(nickUserParts) {
+		case 1:
+			ret = ClientID{
+				Nick: nickHostParts[0],
+				Host: nickHostParts[1],
+			}
+		case 2:
+			ret = ClientID{
+				Nick: nickUserParts[0],
+				User: nickUserParts[1],
+				Host: nickHostParts[1],
+			}
+		default:
+			return ClientID{}, clientIDParseError("More than one '!' char in client id")
+		}
+	default:
+		return ClientID{}, clientIDParseError("More than one '@' char in client id")
+	}
+
+	if ret.Nick == "" {
 		return ClientID{}, clientIDParseError("No nick in client ID.")
 	}
-	if len(parts) == 1 {
-		return ClientID{parts[0], "", ""}, nil
-	} else if len(parts) != 2 {
-		return ClientID{}, clientIDParseError("More than one '!' character")
+	if ret.User != "" && ret.Host == "" {
+		return ClientID{}, clientIDParseError("User but no host in client ID.")
 	}
-	ret.Nick = parts[0]
-	parts = strings.Split(parts[1], "@")
-	if len(parts) != 2 {
-		return ClientID{}, clientIDParseError("Have user field but no host.")
-	}
-	ret.User = parts[0]
-	ret.Host = parts[1]
+
 	return ret, nil
 }
