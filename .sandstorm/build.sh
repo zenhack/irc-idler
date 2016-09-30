640bbe945ba966c870222ecf6cef28e1b8f812f9
David Renshaw <david@sandstorm.io>
2016-09-29 23:27:36 -0400

    Make irc-idler symlink in build.sh so that it is not owned by root.

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -3,6 +3,16 @@ set -euo pipefail
 
 export GOPATH="/opt/app/.sandstorm/gopath"
 
+_srcdir="$GOPATH/src/zenhack.net/go/irc-idler"
+[ -L "$_srcdir" ] || {
+       mkdir -p $(dirname $_srcdir) || true
+       cd $(dirname _srcdir)
+       # Make a symlink back to /opt/app. We do this as
+       # a relative path so we can play with it on the
+       # host machine more easily.
+       ln -s ../../../../..  $_srcdir
+}
+
 # To pull in the dependencies:
 go get -d zenhack.net/go/irc-idler/cmd/sandstorm-irc-idler
 
--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -13,16 +13,4 @@ apt-get update
 apt-get install -y git
 apt-get -t jessie-backports install -y golang
 
-export GOPATH="/opt/app/.sandstorm/gopath"
-_srcdir="$GOPATH/src/zenhack.net/go/irc-idler"
-[ -L "$_srcdir" ] || {
-       mkdir -p $(dirname $_srcdir) || true
-       cd $(dirname _srcdir)
-       # Make a symlink back to /opt/app. We do this as
-       # a relative path so we can play with it on the
-       # host machine more easily.
-       ln -s ../../../../..  $_srcdir
-}
-
-set -euo pipefail
 exit 0
